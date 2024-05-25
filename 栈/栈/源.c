82cfeb506c1d993a285b9e2a098f3dd1d7e1ed36
LAGOM-YOLO-GX330 <76835550+LAGOM-YOLO-GX330@users.noreply.github.com>
2024-05-25 22:45:06 +0800

    Add files via upload

---
+++ 栈/栈/源.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<assert.h>
+typedef int stdata;
+typedef struct stack
+{
+	stdata* a;
+	int top;
+	int capacity;
+}ST;
+bool STempty(ST* ps)
+{
+	return ps->top == 0;
+}
+void STinit(ST* ps)
+{
+	assert(ps);
+	ps->a = NULL;
+	ps->capacity = 0;
+	ps->top = 0;
+}
+void STpush(ST* ps, stdata x)
+{
+	assert(ps);
+	if (ps->capacity == ps->top)
+	{
+		int newcapacity = ps->capacity = 0 ? 4 : ps->capacity * 2;
+		stdata* new = (stdata*)realloc(ps->a, sizeof(newcapacity));
+		if (new == NULL)
+		{
+			perror("STpush realloc fail");
+			return;
+		}
+		ps->a = new;
+		ps->capacity = newcapacity;
+	}
+	ps->a[ps->top++] = x;
+	ps->top++;
+}
+void STdestroy(ST* ps)
+{
+	assert(ps);
+	free(ps->a);
+	ps->a = NULL;
+	ps->capacity = ps->top = 0;
+}
+void STpop(ST* ps)
+{
+	assert(ps);
+	assert(ps->top > 0);
+	ps->top--;
+}
+stdata STTop(ST* ps)
+{
+	assert(ps);
+	return ps->a[ps->top--];
+}
+int STsizeof(ST* ps)
+{
+	assert(ps);
+	return ps->top;
+}
+int main()
+{
+	return 0;
+}
